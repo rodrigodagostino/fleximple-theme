5ededb6378c21be7d013753407fb87c046f650bf
Rodrigo D’Agostino <rodrigo.dagostino@protonmail.com>
2021-07-28 17:28:46 -0300

    Format code

--- src/js/frontend/skip-link-focus-fix.js
+++ src/js/frontend/skip-link-focus-fix.js
@@ -11,13 +11,13 @@
 	if ( isIe && document.getElementById && window.addEventListener ) {
 		window.addEventListener( 'hashchange', function () {
 			const id = location.hash.substring( 1 )
-			if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
+			if ( !/^[A-z0-9_-]+$/.test( id ) ) {
 				return
 			}
 
 			const element = document.getElementById( id )
 			if ( element ) {
-				if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
+				if ( !/^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
 					element.tabIndex = -1
 				}
 

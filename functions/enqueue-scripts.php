a39b8c2e1b404d5b1f76398a03efdf4187a78113
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2022-05-28 23:56:24 -0300

    Store fonts locally

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -1,32 +1,7 @@
 <?php
 /**
- * Register Google Fonts
- */
-function fleximpletheme_google_fonts_url() {
-	$font_families = array();
-	$font_families[] = 'Bitter:ital,wght@0,400;0,700;1,400;1,700';
-	$font_families[] = 'Encode+Sans:ital,wght@0,400;0,600;1,400;1,600';
-
-	$query_args = array(
-		'family' => implode( '&family=', $font_families ),
-		'display' => 'swap',
-	);
-
-	$fonts_url = esc_url( add_query_arg( $query_args, 'https://fonts.googleapis.com/css2' ) );
-	return $fonts_url;
-}
-
-/**
  * Enqueue admin scripts.
  */
-// function fleximpletheme_enqueue_admin_assets() {
-// 	wp_enqueue_style(
-// 		'theme-admin-fonts',
-// 		fleximpletheme_google_fonts_url(),
-// 		array(),
-// 		null
-// 	);
-
 // 	if ( file_exists( get_template_directory_uri() . '/dist/admin-script.js' ) ) {
 // 		wp_enqueue_script(
 // 			'theme-admin-scripts',
@@ -56,17 +31,9 @@ function fleximpletheme_enqueue_customizer_assets() {
 add_action( 'customize_preview_init', 'fleximpletheme_enqueue_customizer_assets' );
 
 /**
- * Enqueue fonts, scripts and styles.
+ * Enqueue scripts and styles.
  */
 function fleximpletheme_enqueue_assets() {
-	wp_enqueue_style(
-		'theme-google-fonts',
-		fleximpletheme_google_fonts_url(),
-		false,
-		null,
-		'all'
-	);
-
 	wp_enqueue_style(
 		'theme-styles',
 		get_template_directory_uri() . '/dist/style.css',
@@ -96,17 +63,9 @@ function fleximpletheme_enqueue_assets() {
 add_action( 'wp_enqueue_scripts', 'fleximpletheme_enqueue_assets', 10 );
 
 /**
- * Enqueue editor fonts and styles.
+ * Enqueue editor styles.
  */
 function fleximpletheme_enqueue_editor_assets() {
-	// Fonts
-	wp_enqueue_style(
-		'theme-editor-google-fonts',
-		fleximpletheme_google_fonts_url(),
-		array(),
-		null
-	);
-
 	// Scripts
 	wp_enqueue_script(
 		'theme-editor-scripts',
@@ -141,13 +100,6 @@ add_action( 'enqueue_block_editor_assets', 'fleximpletheme_enqueue_editor_assets
  */
 function fleximpletheme_enqueue_login_styles() {
 	wp_enqueue_style(
-		'theme-login-google-fonts',
-		fleximpletheme_google_fonts_url(),
-		array(),
-		null
-	);
-
-	wp_enqueue_style(
 		'theme-login-styles',
 		get_template_directory_uri() . '/dist/login-style.css',
 		false,
@@ -156,23 +108,3 @@ function fleximpletheme_enqueue_login_styles() {
 }
 //This loads the function above on the login page
 add_action( 'login_enqueue_scripts', 'fleximpletheme_enqueue_login_styles' );
-
-
-/**
- * Defer loading of stylesheet.
- */
-function fleximpletheme_defer_css( $html, $handle ) {
-    $deferred_stylesheets = apply_filters( 'fleximpletheme_deferred_stylesheets', array() );
-
-    if ( in_array( $handle, $deferred_stylesheets, true ) ) {
-        return str_replace( 'media=\'all\'', 'media="print" onload="this.media=\'all\'"', $html );
-    } else {
-        return $html;
-    }
-}
-add_filter( 'style_loader_tag', 'fleximpletheme_defer_css', 10, 2 );
-
-add_filter( 'fleximpletheme_deferred_stylesheets', function( $handles ) {
-    $handles[] = 'theme-google-fonts';
-    return $handles;
-}, 10, 1 );
